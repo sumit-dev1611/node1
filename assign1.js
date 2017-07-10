73149e748a3579a5bc0a2bc101e5874de7afd3db
sumitdev <sumit-dev1611>
2017-07-10 19:05:47 +0530

    v2.0

--- assign1.js
+++ assign1.js
@@ -1,104 +1,67 @@
     var mongoose = require('mongoose');
     var async = require('async');
+    var md5 = require('md5');
     var conn = mongoose.createConnection('mongodb://admin:123@ds151232.mlab.com:51232/sumit');
-    var post_schema1 = mongoose.Schema({}, {
-        strict: false,
+    var user_schema = mongoose.Schema({
+        firstname: String,
+        lastname: String,
+        email: String,
+        password: String
+    }, {
+        strict: true,
         collection: 'Users'
     });
-    var post_schema2 = mongoose.Schema({}, {
-        strict: false,
+    var userprofile_schema = mongoose.Schema({
+        User_id: String,
+        dob: String,
+        Mobile_no: Number
+    }, {
+        strict: true,
         collection: 'UsersProfile'
     });
-    var uid;
-    var post1 = conn.model('post1', post_schema1);
-    var post2 = conn.model('post2', post_schema2);
-    for (var i = 1; i <= 5; i++) {
-        uid = 0;
-        (function(i) {
-            post1.find({
-                firstname: /ab/i
-            }, function(err, result) {
+    var user = conn.model('user', user_schema);
+    var userprofile = conn.model('userprofile', userprofile_schema);
+    var userdetail = [
+        { firstname: 'John', lastname: 'cena', email: 'john8798@gmail.com', password: 'hfgh', dob: '06/12/1912', Mobile_no: 365154366 },
+        { firstname: 'Peter', lastname: 'parker', email: 'peter@hotmail.com', password: 'hgfhgf', dob: '15/11/1972', Mobile_no: 254654369 },
+        { firstname: 'Amy', lastname: 'walters', email: 'peter@hotmail.com', password: 'hjgdfgdf', dob: '09/09/1956', Mobile_no: 454515436 },
+        { firstname: 'Hannah', lastname: 'montanah', email: 'hannah74@gmail.com', password: 'fdgfdg', dob: '17/06/1942', Mobile_no: 996515436 },
+        { firstname: 'Michael', lastname: 'jackson', email: 'jack378@hotmail.com', password: 'gdfgdfg', dob: '22/03/1935', Mobile_no: 885154363 },
+    ];
+
+
+    async.forEachOf(userdetail, function(data, key, callback) {
+            var new_data = new user({
+                firstname: data.firstname,
+                lastname: data.lastname,
+                email: data.email,
+                password: md5(data.password)
+            });
+            new_data.save(function(err, data1) {
                 if (err) {
                     console.log(err);
                     process.exit();
-                }
-                if (result.length == 0) {
-                    var new_post1 = new post1({
-                        firstname: 'AB',
-                        lastname: 'D ' + i,
-                        email: 'abd@sa',
-                        password: 'proteas'
+                } else if (data1) {
+                    var new_data1 = new userprofile({
+                        User_id: data1._id,
+                        dob: data.dob,
+                        Mobile_no: data.Mobile_no
                     });
-                    new_post1.save(function(err, data) {
-                        //save done
+
+                    new_data1.save(function(err, data) {
                         if (err) {
                             console.log(err);
                             process.exit();
                         }
-                        console.log('Post1 Saved')
-                        uid = data._id;
-                    });
-                } else {
-                    var new_post1 = new post1({
-                        firsdtname: 'ms',
-                        lastname: 'D ' + i,
-                        email: 'msd@ind',
-                        password: 'mahi'
-                    });
 
-                    new_post1.save(function(err, data) {
-                        //save done
-                        if (err) {
-                            console.log(err);
-                            process.exit();
-                        }
-                        console.log('Post1 Saved');
-                        uid = data._id;
                     });
-
                 }
+                callback();
             });
-        })(i);
-
-        (function(i) {
-            post2.find({
-                title: /UsersProfile/i
-            }, function(err, result) {
-                if (err) {
-                    console.log(err);
-                    process.exit();
-                }
-                if (!result || result.length == 0) {
-                    var new_post2 = new post2({
-                        user_id: uid,
-                        dob: '12/12/12',
-                        Mobile_no: 173247578
-                    });
-                    new_post2.save(function(err) {
-                        //save done
-                        if (err) {
-                            console.log(err);
-                            process.exit();
-                        }
-                        console.log('Post2 Saved')
-                    });
-                } else {
-                    var new_post2 = new post2({
-                        user_id: uid,
-                        dob: '7/7/9' + i,
-                        Mobile_no: 874654780 + i
-                    });
-
-                    new_post2.save(function(err) {
-                        //save done
-                        if (err) {
-                            console.log(err);
-                            process.exit();
-                        }
-                        console.log('Post2 Saved')
-                    });
-                }
-
-            });
-        })(i);
-    }
+        },
+        function(err) {
+            if (err) {
+                console.log(err);
+                process.exit();
+            }
+        });
